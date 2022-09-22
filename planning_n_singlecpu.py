b8513bf80c3cf6a0b3526340825ee611bcae6e39
YouSonicAI <queenie.liu@yousonic.ai>
2022-09-22 23:48:24 +0800

    last normal

--- planning_n_singlecpu.py
+++ planning_n_singlecpu.py
@@ -5,6 +5,7 @@ import os
 import warnings
 import torch
 import argparse
+from utils.others import process_input, process_xy, process_xy_back
 
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
@@ -14,7 +15,50 @@ import time
 ACCELERATE_SEARCH = 1
 SEARCH_SPEED = 10.0
 
+def model_generator(avm, data, start):
+    city_name_, data_ = process_input(data)
 
+    ## filter the data
+
+    x, y = process_xy(data_)
+    measurements = data_
+    initial_state_mean = [measurements[0, 0],
+                          0,
+                          measurements[0, 1],
+                          0]
+
+    transition_matrix = [[1, 1, 0, 0],
+                         [0, 1, 0, 0],
+                         [0, 0, 1, 1],
+                         [0, 0, 0, 1]]
+
+    observation_matrix = [[1, 0, 0, 0],
+                          [0, 0, 1, 0]]
+
+    # kf1 = KalmanFilter(transition_matrices=transition_matrix,
+    #                    observation_matrices=observation_matrix,
+    #                    initial_state_mean=initial_state_mean)
+    #
+    # kf1 = kf1.em(measurements, n_iter=5)
+    # (smoothed_state_means, smoothed_state_covariances) = kf1.smooth(measurements)
+    #
+    # kf2 = KalmanFilter(transition_matrices=transition_matrix,
+    #                    observation_matrices=observation_matrix,
+    #                    initial_state_mean=initial_state_mean,
+    #                    observation_covariance=10 * kf1.observation_covariance,
+    #                    em_vars=['transition_covariance', 'initial_state_covariance'])
+    #
+    # kf2 = kf2.em(measurements, n_iter=5)
+    # (smoothed_state_means, smoothed_state_covariances) = kf2.smooth(measurements)
+    # data_k = process_xy_back(smoothed_state_means[:, 0], smoothed_state_means[:, 2])
+    #
+    # ## dfs search
+    # # avm = ArgoverseMap()
+    # candidate_centerlines = avm.get_candidate_centerlines_for_traj(data_k[0:19], city_name_, viz=False)
+    # dfs = candidate_centerlines
+    #
+    # ## dfs pruning
+    # dfs_save_p = []
 
 
 if __name__ == "__main__":
@@ -53,7 +97,7 @@ if __name__ == "__main__":
                 path, name_ext = os.path.split(path_name_ext)
                 name, ext = os.path.splitext(name_ext)
                 time_begin = time.time()
-                # save_centerline, generate_traj = model_generator(avm, afl_.seq_df, 20)
+                save_centerline, generate_traj = model_generator(avm, afl_.seq_df, 20)
                 # data_dict = {"save_centerline": save_centerline, "generate_traj": generate_traj}
                 data_dict = {}
                 torch.save(data_dict, os.path.join(save_dir, name + ".path"))
